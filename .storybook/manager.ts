0a5d749253b8a3dd04e6139e1b6f942a606f2131
wewakekumar <vivekrajwe1@gmail.com>
2023-11-26 11:56:27 +0530

    added storybook

---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import pazTheme from './pazTheme';
+
+addons.setConfig({
+  theme: pazTheme,
+});
---
+++ .storybook/pazTheme.ts
@@ -0,0 +1,14 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Pazcare',
+  brandUrl: 'https://app.pazcare.com/login',
+  brandImage: 'https://app.pazcare.com/static/media/newPazcareLogo.79360419.svg',
+  brandTarget: '_self',
+
+  colorPrimary: '#e6377a',
+  colorSecondary: '#46077a',
+
+  appBg: "#red"
+});
